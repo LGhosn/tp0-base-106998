abe88c6ddc1801c094d4836cea0f501c6e2bf516
LGhosn <lghosn@fi.uba.ar>
2025-03-25 20:34:16 -0300

    done?

--- server/common/server.py
+++ server/common/server.py
@@ -1,61 +1,72 @@
 import socket
 import logging
 import signal
+import multiprocessing
+from time import sleep
 from common.bet_center import BetCenterListener, BetCenter
 from common.utils import BET_FLAG, END_FLAG, has_won, load_bets, store_bets
 
 class Server:
     def __init__(self, port, listen_backlog, cant_clientes):
-        # Initialize server socket
         self.cant_clientes = cant_clientes
         self._server_socket = BetCenterListener.bind('', port, listen_backlog)
-        self._server_running = True
-        
+        self._server_running = multiprocessing.Value('b', True)
+        self.agencies = {}
+        self.end_flags = multiprocessing.Barrier(cant_clientes)
+
         signal.signal(signal.SIGTERM, self.__server_handle_sigterm)
         signal.signal(signal.SIGINT, self.__server_handle_sigterm)
 
     def run(self):
-        agencies = {}
-        while self._server_running and len(agencies) < self.cant_clientes:
+        while self._server_running.value and len(self.agencies) < self.cant_clientes:
             client_sock = self.__accept_new_connection()
             if client_sock:
-                    self.__handle_client_connection(client_sock)
-                    agencies[client_sock.agency] = client_sock
+                client_process = multiprocessing.Process(
+                    target=self.__handle_client_connection,
+                    args=(client_sock, self.end_flags)
+                )
+                client_process.start()
+                self.agencies[client_sock.agency] = (client_process, client_sock)
 
-        # Close server socket
         self._server_socket.close()
+        logging.info(f"esperando a que terminen {self.end_flags.n_waiting}")
+        
+        for p, _ in self.agencies.values():
+            p.join()
 
-        if self._server_running:
-            logging.info("action: sorteo | result: success")
+        logging.info("Todos terminaron")
+        
+        self.__process_results()
 
-            winners = {}
-            for bet in load_bets():
-                if has_won(bet):
-                    if bet.agency not in winners:
-                        winners[bet.agency] = []
-                    winners[bet.agency].append(int(bet.document))
-            
-            #envio los ganadores correspondientes a cada agencia
-            for agency, socket in agencies.items():
-                socket.send_winners(winners.get(agency, []))
+        # Cerrar procesos hijos y sockets
+        for _, s in self.agencies.values():
+            s.close()
+        
+        sleep(5)
 
-        for socket in agencies.values():
-            socket.close()
+    def __process_results(self):
+        logging.info("action: sorteo | result: success")
 
+        winners = {}
+        for bet in load_bets():
+            if has_won(bet):
+                winners.setdefault(bet.agency, []).append(int(bet.document))
+
+
+        # Enviar ganadores a cada cliente
+        for agency, (_, client_sock) in self.agencies.items():
+            try:
+                client_sock.send_winners(winners.get(agency, []))
+            except Exception as e:
+                logging.error(f"action: send_winners | result: fail | error: {e}")
 
     def __server_handle_sigterm(self, _signal, _frame):
-        """
-        Maneja la señal SIGTERM para cerrar el servidor de forma segura
-        """
         logging.info("action: shutdown | result: success | message: SIGTERM received, shutting down server...")
-        self._server_running = False
-    
-    def __handle_client_connection(self, client_sock: BetCenter):
-        """
-        Read message from a specific client socket and closes the socket
+        self._server_running.value = False  
 
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
+    def __handle_client_connection(self, client_sock: BetCenter, end_flags):
+        """
+        Maneja la comunicación con un cliente en un proceso separado.
         """
         try:
             flag = client_sock.recv()
@@ -65,24 +76,22 @@ class Server:
                 store_bets(data)
             else:
                 logging.info(f"action: fin_apuestas | result: success")
+            
+            try:
+                end_flags.wait(timeout=30)
+                logging.info("cliente sincronizado")
+            except multiprocessing.BrokenBarrierError:
+                logging.error("Barrera rota")
         except OSError as e:
-            logging.error("action: apuesta_recibida | result: fail | error: {e}")
+            logging.error(f"action: apuesta_recibida | result: fail | error: {e}")
+        finally:
+            client_sock.close()
 
     def __accept_new_connection(self) -> BetCenter:
-        """
-        Accept new connections
-
-        Function blocks until a connection to a client is made.
-        Then connection created is printed and returned
-        """
-
-        # Connection arrived
         logging.info('action: accept_connections | result: in_progress')
         try:
             c, addr = self._server_socket.accept()
             logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
             return c
-        except socket.timeout:
-            return None
-        except OSError as e:
+        except (socket.timeout, OSError):
             return None
